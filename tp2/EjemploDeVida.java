5f445dd4fffb5f6fd90448a961ca487ff215987f
Sofia Suvercase <ssuvercase@everfi.com>
2019-09-11 09:59:23 -0300

    Tp 2

--- tp2/EjemploDeVida.java
+++ tp2/EjemploDeVida.java
@@ -3,14 +3,18 @@ public class EjemploDeVida{
 
         Persona sofia;
 
-        sofia = new Persona ("Sofia", "Mujer", "39267083", 2.8, 0.50);
+        sofia = new Persona ("Sofia", "Mujer", 38432153, 2.8, 0.50);
 
-        System.out.print(sofia.toString());
+        System.out.print(sofia.toString()); //aqui se observa que sofia es un nuevo objeto de tipo persona.
 
         //d1)Alimentar a Sofia 1.0 es un valor, se le dice literal = son variables sin nombre.
         sofia.alimentar(1.0);
         //d2) crecer
         sofia.crecer(0.5);
+        sofia.cumplirAnos();
+
+        System.out.print(sofia.toString());//lo que ocurre quí es que el objeto Sofia adquiere nuevos valores al implementar los métodos
+        //alimentar(), crecer() y cumplirAnos()
 
     }
 }
---
+++ tp2/OtrasVidasNoTanEjemplares.java
@@ -0,0 +1,12 @@
+public class OtrasVidasNoTanEjemplares{
+    public static void main(String args[]){
+
+
+        Persona mauricio = new Persona ("Mauricio", "Hombre", 33999776, 70, 1.70, 50, true);
+        Persona marcos = new Persona ("Mauricio", "Hombre", 33999776, 70, 1.70);
+        Persona eugenia = new Persona ("Mauricio", "Hombre", 33999776, 70, 1.70);
+        Persona elisa = new Persona ("Mauricio", "Hombre", 33999776, 70, 1.70);
+        System.out.print(mauricio.toString());
+
+    }
+}
